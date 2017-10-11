acc630c1fafc251cfffd910c5628c8ef40d067ba
Alec Missine <alec_missine@yahoo.com>
2017-10-11 12:57:39 -0400

    Update devDependencies

--- bin/lnet.js
+++ bin/lnet.js
@@ -1,66 +1,10 @@
 #!/usr/bin/env node
 
-const context = require("../conf/context.json"),
-  readline = require("readline"),
-  rl = readline.createInterface({ input: process.stdin, output: process.stdout })
+const context = require("../conf/context.json") //color = require("../etc/color.json")
+const readline = require("readline")
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
 
-var argv = process.argv.slice(2)
-  , path = require("path")
-//  , Runner = require("../lib/tap-runner")
-
-  , nopt = require("nopt")
-
-  , knownOpts =
-    { cover: [path, false]
-    , "cover-dir": path
-    , stderr: Boolean
-    , stdout: Boolean
-    , diag: Boolean
-    , version: Boolean
-    , tap: Boolean
-    , timeout: Number
-    , gc: Boolean
-    , debug: Boolean
-    , "debug-brk": Boolean
-    , strict: Boolean
-    , harmony: Boolean
-    }
-
-  , shorthands =
-    // debugging 1: show stderr
-    { d: ["--stderr"]
-    // debugging 2: show stderr and tap
-    , dd: ["--stderr", "--tap"]
-    // debugging 3: show stderr, tap, AND always show diagnostics.
-    , ddd: ["--stderr", "--tap", "--diag"]
-    , "expose-gc": ["--gc"]
-    , g: ["--gc"]
-    , e: ["--stderr"]
-    , t: ["--timeout"]
-    , o: ["--tap"]
-    , c: ["--cover"]
-    , v: ["--version"]
-    , "?": ["--help"]
-    , h: ["--help"]
-    }
-
-  , defaults =
-    { cover: "./lib"
-    , "cover-dir": "./coverage"
-    , stderr: process.env.TAP_STDERR !== '0'
-    , tap: process.env.TAP
-    , diag: process.env.TAP_DIAG
-    , timeout: +process.env.TAP_TIMEOUT || 30
-    , gc: false
-    , debug: false
-    , "debug-brk": false
-    , strict: false
-    , harmony: false
-    , version: false
-    , help: false }
-
-  , options = nopt(knownOpts, shorthands)
-
+/*
 if (options.version) {
   console.log(require("../package.json").version)
   process.exit(0)
@@ -91,9 +35,10 @@ Options:
 
 Please report bugs!  https://github.com/isaacs/node-tap/issues
 
-*/}.toString().split(/\n/).slice(1, -1).join("\n"))
+*//*}.toString().split(/\n/).slice(1, -1).join("\n"))
   process.exit(0)
 }
+*/
 
 // Schedule SSH connection attempts for all hubs
 context.hubs.forEach( (hub) => {
@@ -105,57 +50,5 @@ rl.on("close", () => { console.log("\nExiting\n"); process.exit(0) })
 function loop(prompt) { rl.question(prompt, (command) => {
   console.log(`Unknown command %s${command}%s ignored`, "\x1b[31m\x1b[5m", "\x1b[0m")
   loop(prompt)})                                      // FgRed   Blink      Reset
-}; loop("\nEnter command to execute or press Ctrl-D to exit\n> ")
+}; loop("\n\x1b[32mEnter command to execute or press Ctrl-D to exit\n>\x1b[0m ")
 
-/*
-Object.keys(defaults).forEach(function (k) {
-  if (!options.hasOwnProperty(k)) options[k] = defaults[k]
-})
-
-// other tests that might rely on these
-if (options.diag) process.env.TAP_DIAG = true
-if (options.tap) process.env.TAP = true
-if (options.timeout) process.env.TAP_TIMEOUT = options.timeout
-
-var r = new Runner(options)
-  , TapProducer = require("../lib/tap-producer")
-
-if (options.tap || options.diag) {
-  r.pipe(process.stdout)
-} else {
-  r.on("file", function (file, results, details) {
-    var s = (details.ok ? "" : "not ") + "ok "+results.name
-      , n = details.pass + "/" + details.testsTotal
-      , dots = new Array(Math.max(1, 60 - s.length - n.length)).join(".")
-    console.log("%s %s %s", s, dots, n)
-    if (details.ok) {
-      if (details.skip) {
-        console.log("  skipped: %s", details.skipTotal)
-      }
-    } else {
-      // console.error(details)
-      console.log("    Command: %s", results.command)
-      console.log("    " + TapProducer.encode(details.list)
-                  .split(/\n/).join("\n    "))
-    }
-  })
-  r.on("end", function () {
-    //console.log(r)
-    var s = "total"
-      , n = r.results.pass + "/" + r.results.testsTotal
-      , dots = new Array(60 - s.length - n.length).join(".")
-      , ok = r.results.ok ? "ok" : "not ok"
-    console.log("%s %s %s\n\n%s", s, dots, n, ok)
-    if (r.doCoverage) {
-      console.error( "\nCoverage: %s\n"
-                   , path.resolve(r.coverageOutDir, "index.html") )
-    }
-  })
-}
-
-
-
-r.on("end", function () {
-  process.exit(r.results.ok ? 0 : 1)
-})
-*/
