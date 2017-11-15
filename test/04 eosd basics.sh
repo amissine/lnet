df81a57f8a1fccaf8f5906027f72a6ed18d7800b
Alec Missine <alec_missine@yahoo.com>
2017-11-15 17:24:21 -0500

    Added test/rc/04/config.json

--- test/04 eosd basics.sh
+++ test/04 eosd basics.sh
@@ -2,17 +2,39 @@
 #
 # See also: https://tiswww.case.edu/php/chet/bash/bashref.html
 
+TEST_DATA_DIR=test/rc/04
+
 # Configure eosd data-dir
 configure_eosd_data_dir() {
   echo "Configuring eosd data-dir $1"
-  mkdir -p $1
+  pushd $TEST_DATA_DIR
+
+  rm genesis.json; rm o.json; rm *.dot; rm -rf tn_data_?
+
+  cp $HOME/product/eos/build/genesis.json .
+  $HOME/product/eos/build/programs/launcher/launcher -p3 -o o.json
+  ./configure
+ 
+  rm genesis.json; rm o.json; rm *.dot; rm -rf tn_data_?
+
+  popd; return   # work in progress
+
+  mkdir $1; popd # ready to run the test
+}
+
+# Skip the rest of the test
+work_in_progress() {
+  echo "------ work in progress ------"; popd; exit
 }
 
 pushd "$HOME/project/lnet"
 
 # If eosd data-dir does not exist, configure it
-EOSD_DATA_DIR="test/rc/04/$HOSTNAME"
-[[ -d "$EOSD_DATA_DIR" ]] || configure_eosd_data_dir "$EOSD_DATA_DIR"
+EOSD_DATA_DIR="$TEST_DATA_DIR/$HOSTNAME"
+[[ -d "$EOSD_DATA_DIR" ]] || configure_eosd_data_dir "$HOSTNAME"
+
+# Allow for work in progress
+[[ -d "$EOSD_DATA_DIR" ]] || work_in_progress
 
 # Connect to the hub and run the test
 cp test/rc/04\ eosd\ basics.json conf/context.json
