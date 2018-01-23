374df0f31a1c99fd984728cf352139ec187738a8
Alec Missine <alec_missine@yahoo.com>
2018-01-23 18:21:47 -0500

    Create makefile to install Cloud Trust Ltd

--- test/rc/05/ctl_init.sh
+++ test/rc/05/ctl_init.sh
@@ -12,3 +12,28 @@ while true; do
     log "$line"
   else break; fi
 done
+
+: <<'EOF_hub_history'
+   70  groupadd ctl_leaves
+   71  useradd -d /home/ctl -s /bin/bash -g ctl_leaves ctl
+   72  ls -la /home/
+   73  mkdir -p /home/ctl
+   74  chown ctl /home/ctl
+   75  chgrp ctl_leaves /home/ctl
+   76  su ctl
+
+    2  cd $HOME
+    3  mkdir project
+    4  cd project/
+    6  ln -s /home/alec/project/lnet/ lnet
+    9  ls -lLa lnet
+   12  cd ~/
+   13  mkdir .ssh
+   14  cd .ssh/
+   15  cp /home/alec/.ssh/id_rsa.pub .
+   16  cp /home/alec/id_rsa.pub .
+   17  mv id_rsa.pub authorized_keys
+   18  ls -la
+   19  cd ..
+   21  chmod 700 .ssh/
+EOF_hub_history
