67b3e59192dccc8b37a62eb7d6b5d95c2ed326b7
Alec Missine <alec_missine@yahoo.com>
2018-02-21 05:24:10 -0500

    Run test/06 Cloud 1, Kiev - Miami.sh from macOS

--- test/06 Cloud 1, Kiev - Miami.sh
+++ test/06 Cloud 1, Kiev - Miami.sh
@@ -1,25 +1,36 @@
-#!/usr/bin/env bash
+#!/usr/local/bin/bash
+#
+# Copyright 2017 Alec Missine (support@minetats.com) 
+#                and the Arbitrage Logistics International (ALI) project
+#
+# The ALI Project licenses this file to you under the Apache License, version 2.0
+# (the "License"); you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
 # === 06 Cloud 1, Kiev - Miami.sh ===
-# Test joining the Cloud Trust Ltd as a leaf
+# Test the process of joining the Cloud Trust Ltd as a leaf
+#
+# See also:
+#   https://docs.google.com/document/d/1JPzTa7IXEQL0NZLoO5leCNj40e7yy1dFNMiqTtBNH2o/
+#   https://www.gnu.org/software/bash/manual/bashref.html
+#   https://tiswww.case.edu/php/chet/bash/bashref.html
 
 unset CDPATH  # To prevent unexpected `cd` behavior.
-
-# Print the embedded test plan to stdout.
-printTestPlan() {
-  sed -n -e $'/^: <<\'EOF_TEST_PLAN\'/,/^EOF_TEST_PLAN/ { s///; t\np;}' "$BASH_SOURCE"
-}
 
 #----------------------------------
 pushd "$HOME/project/lnet"
 
-# Ignore the testplan
-#eval "`printTestPlan`" | bin/run.js "../test/rc/06 Cloud 1, Kiev - Miami.json"
 #
 # IMPORTANT: the first time, we MUST connect to the hub manually - to add it to known_hosts
 #
-pipe="/tmp/${USER}_mia-hub.in"
-rm $pipe 2>/dev/null
-mkfifo $pipe
+pipe="/tmp/${USER}_mia-hub.in"; rm $pipe 2>/dev/null; mkfifo $pipe
 cat $pipe | bin/run.js "../test/rc/06 Cloud 1, Kiev - Miami.json" >> "/tmp/${USER}_mia-hub.out" 2>&1 &
 EXIT_CODE=$?
 [[ $EXIT_CODE == 0 ]] && echo "TEST PASSED" || echo "TEST FAILED, EXIT_CODE=$EXIT_CODE"
@@ -27,8 +38,3 @@ EXIT_CODE=$?
 
 popd
 #----------------------------------
-: <<'EOF_TEST_PLAN'
-sleep 60
-echo "hub1 stop heartbeat"
-sleep 180
-EOF_TEST_PLAN
