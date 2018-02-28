4c175e27866b53537bf26547844d8253f249a372
Alec Missine <alec_missine@yahoo.com>
2018-02-28 18:34:38 -0500

    Refactoring

---
+++ test/00 localhost.sh
@@ -0,0 +1,59 @@
+#!/usr/local/bin/bash
+#
+# Copyright 2017 Alec Missine (support@minetats.com) 
+#                and the Arbitrage Logistics International (ALI) project
+#
+# The ALI Project licenses this file to you under the Apache License, version 2.0
+# (the "License"); you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software distributed
+# under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# === 00 localhost.sh ===
+# The leaf - hub SSH connectivity on localhost.
+#
+# See also:
+#   https://docs.google.com/document/d/1JPzTa7IXEQL0NZLoO5leCNj40e7yy1dFNMiqTtBNH2o/
+#   https://www.gnu.org/software/bash/manual/bashref.html
+#   https://tiswww.case.edu/php/chet/bash/bashref.html
+
+kTHIS_NAME=${BASH_SOURCE##*/} # whatever is left after the last '/' in the script name
+
+unset CDPATH  # To prevent unexpected 'cd' behavior.
+
+# --- Begin: STANDARD HELPER FUNCTIONS
+
+declare logfile="/tmp/test01.log"
+
+log() {
+  local f='-Ins' timestamp=$([[ `uname` == "Darwin" ]] && gdate $f || date $f)
+  echo "$timestamp $1" >> $logfile
+}
+
+die() { echo "$kTHIS_NAME: ERROR: ${1:-"ABORTING due to unexpected error."}" 1>&2; exit ${2:-1}; }
+dieSyntax() { echo "$kTHIS_NAME: ARGUMENT ERROR: ${1:-"Invalid argument(s) specified."} Use -h for help." 1>&2; exit 2; }
+
+# Print the embedded test plan to stdout.
+printTestPlan() {
+  sed -n -e $'/^: <<\'EOF_TEST_PLAN\'/,/^EOF_TEST_PLAN/ { s///; t\np;}' "$BASH_SOURCE"
+}
+
+#----------------------------------
+[ `pwd` != "$HOME/project/lnet" ] && die "Please run this script from $HOME/project/lnet"
+
+eval "`printTestPlan`" | bin/lnet.js "../test/rc/00 localhost.json"
+EXIT_CODE=$?
+[[ $EXIT_CODE == 0 ]] && echo "TEST PASSED" || echo "TEST FAILED, EXIT_CODE=$EXIT_CODE"
+
+#----------------------------------
+: <<'EOF_TEST_PLAN'
+sleep 25
+echo "hub1 stop heartbeat"
+sleep 70
+EOF_TEST_PLAN
+
