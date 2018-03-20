28d099aa28d452fb8b045dc22f89864fbc1eafe0
Alec Missine <alec_missine@yahoo.com>
2018-03-20 18:47:40 -0400

    Two hubs test 8

--- test/05 Two hubs.sh
+++ test/05 Two hubs.sh
@@ -36,20 +36,43 @@ log() {
 die() { echo "$kTHIS_NAME: ERROR: ${1:-"ABORTING due to unexpected error."}" 1>&2; exit ${2:-1}; }
 dieSyntax() { echo "$kTHIS_NAME: ARGUMENT ERROR: ${1:-"Invalid argument(s) specified."} Use -h for help." 1>&2; exit 2; }
 
-# Print the embedded test plan to stdout.
-printTestPlan() {
-  sed -n -e $'/^: <<\'EOF_TEST_PLAN\'/,/^EOF_TEST_PLAN/ { s///; t\np;}' "$BASH_SOURCE"
+# Print the embedded help info to stdout.
+printUsage() {
+  sed -n -e $'/^: <<\'EOF_HELP\'/,/^EOF_HELP/ { s///; t\np;}' "$BASH_SOURCE"
+}
+
+# --- Begin: TEST FUNCTIONS
+
+runAll() {
+  local conf="$HOME/project/lnet/$1"
+  echo "runAll: using configuration file $conf"
+  local output=$(node <<EOF_JS
+const conf = require("$conf")
+const util = require("util")
+console.log(util.inspect(conf))
+EOF_JS
+)
+  echo "$output"
 }
 
 #----------------------------------
 [ `pwd` != "$HOME/project/lnet" ] && die "Please run this script from $HOME/project/lnet"
 
-while getopts ':km' opt; do  # $opt will receive the option *letters* one by one; a trailing : means that an arg. is required, reported in $OPTARG.
+while getopts ':ahklm' opt; do  # $opt will receive the option *letters* one by one; a trailing : means that an arg. is required, reported in $OPTARG.
   [[ $opt == '?' ]] && dieSyntax "Unknown option: -$OPTARG"
   [[ $opt == ':' ]] && dieSyntax "Option -$OPTARG is missing its argument."
   case "$opt" in
+    a) # run the test on all the boxes
+      runAll "test/rc/05\ Two\ hubs.json"; exit 0
+      ;;
+    h) # print usage
+      printUsage; exit 0
+      ;;
     k) # kiev leaf
       config="../test/rc/05 Two hubs, Kiev leaf.json"
+      ;;
+    l) # kiev hub, and the leaf on it connects to both kiev-hub and mia-hub
+      config="../test/rc/05 Two hubs, kiev hub.json"
       ;;
     m) # mia leaf
       config="../test/rc/05 Two hubs, mia leaf.json"
@@ -59,7 +82,7 @@ while getopts ':km' opt; do  # $opt will receive the option *letters* one by one
       ;;
   esac
 done
-[ -z "$config" ] && dieSyntax "Please specify either -k or -m option."
+[ -z "$config" ] && dieSyntax "Please specify the configuration option."
 
 pipe="/tmp/${USER}_2hubs.in"; rm $pipe 2>/dev/null; mkfifo $pipe; rm "/tmp/${USER}_2hubs.out"
 cat $pipe | bin/lnet.js "$config" >> "/tmp/${USER}_2hubs.out" 2>&1 &
@@ -69,3 +92,16 @@ EXIT_CODE=$?
 # admin@kiev-leaf2 ~ $ echo "hub1 exit" >> /tmp/admin_2hubs.in
 
 #----------------------------------
+: <<'EOF_HELP'
+
+  The configuration options are:
+
+    -a  try and run the test on all the boxes that have been configured for the test
+    -h  print this message to stdout
+    -k  start the test on localhost as kiev-leaf
+    -l  start the test on the kiev-hub localhost as kiev-leaf and mia-leaf
+    -m  start the test on localhost as mia-leaf
+
+  See also: https://docs.google.com/document/d/1JPzTa7IXEQL0NZLoO5leCNj40e7yy1dFNMiqTtBNH2o
+
+EOF_HELP
